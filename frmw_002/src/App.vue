298c6a4f71ec0276c88ca50aaa425c68a206241d
Ian <mrexox@outlook.com>
2020-03-22 17:29:20 +0300

    FRMW_002

---
+++ frmw_002/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h2>Vue example</h2>
+    <input v-model="value" />
+    <br />
+    <input v-model="value" />
+    <br />
+    <code>
+    <pre>
+
+&lt;template&gt;
+  &lt;div id="app"&gt;
+    &lt;h1&gt;Vue example&lt;/h1&gt;
+    &lt;input v-model="value" /&gt;
+    &lt;br /&gt;
+    &lt;input v-model="value" /&gt;
+  &lt;/div&gt;
+&lt;/template&gt;
+
+&lt;script&gt;
+export default {
+  name: 'app',
+  data() {
+    return {
+        value: ''
+    }
+  },
+  components: {}
+}
+&lt;/script&gt;
+
+</pre>
+    </code>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+        value: ''
+    }
+  },
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
